588d6cb920058a6e87c78c274b2764e26e203ad7
Mike Shippy <mikeship04@gmail.com>
2022-06-07 16:13:52 -0700

    commit tree 77e911476ae7512ecb8dd3c5fd1b0c7cad618c82

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Write your solution in this file
+
+var customerName = 'bob'
+
+
+function upperCaseCustomerName(name) {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'george'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'timothy';
+}
